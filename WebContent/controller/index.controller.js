54e0f0a1135c29e529ec36bf2bbaccb8f7fa49b9
Anhoni <ankitsoni231294@gmail.com>
2019-11-19 02:29:20 +0530

    veiw working

--- WebContent/controller/index.controller.js
+++ WebContent/controller/index.controller.js
@@ -1,38 +1,20 @@
-sap.ui.controller("sales_repo.index", {
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf sales_repo.index
-*/
-//	onInit: function() {
-//
-//	},
+	return Controller.extend("sales_repo.controller.index", {
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf sales_repo.index
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+		onShowHello : function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf sales_repo.index
-*/
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf sales_repo.index
-*/
-//	onExit: function() {
-//
-//	}
+			// show message
+			MessageToast.show(sMsg);
+		}
+	});
 
 });
